7b880ab4bd67f1ea30666ab634d2023ab3a150ae
drawsiege <yusukebalaoy@gmail.com>
2023-04-06 09:23:24 +0800

    fix errors

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -6,7 +6,7 @@ import { signIn } from 'next-auth/react';
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
 
-const auth = () => {
+const Auth = () => {
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
@@ -106,4 +106,4 @@ const auth = () => {
   )
 }
 
-export default auth
+export default Auth
